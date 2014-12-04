cf38e7380232e2ea825aa1f3441c5ffac822ffaf
irinaColgiu <irina.colgiu@gmail.com>
2014-12-04 13:56:47 +0000

    Added licence headers

--- header_parser/bam_hparser.py
+++ header_parser/bam_hparser.py
@@ -1,11 +1,11 @@
 """
-#################################################################################
-
 Copyright (C) 2013, 2014  Genome Research Ltd.
 
 Author: Irina Colgiu <ic4@sanger.ac.uk>
 
-This program is free software: you can redistribute it and/or modify
+This program is part of metadata-check.
+
+metadata-check is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
 published by the Free Software Foundation, either version 3 of the
 License, or (at your option) any later version.
@@ -15,9 +15,13 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Affero General Public License for more details.
 
-Created on Nov 3, 2014
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+
+This file has been created on Nov 3, 2014.
 """
+
 import re
 import pysam
 from collections import namedtuple
--- header_parser/irods_bam_hparser.py
+++ header_parser/irods_bam_hparser.py
@@ -1,21 +1,27 @@
 """
-Created on Dec 02, 2014
+Copyright (C) 2014  Genome Research Ltd.
+
 Author: Irina Colgiu <ic4@sanger.ac.uk>
 
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
-Foundation; either version 3 of the License, or (at your option) any later
-version.
+This program is part of metadata-check.
 
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-details.
+metadata-check is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-You should have received a copy of the GNU General Public License along with
-this program. If not, see <http://www.gnu.org/licenses/>.
- 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+This file has been created on Nov 3, 2014.
 """
+
 
 import subprocess
 import config
