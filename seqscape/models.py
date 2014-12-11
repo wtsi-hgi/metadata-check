d86afc6545015d3bcf02d35df5c9c5dd0ac730ae
irinaColgiu <irina.colgiu@gmail.com>
2014-12-11 13:20:27 +0000

    Added __eq__ method to the SQLAlchemy models so that the equality of 2 objs is decided based on name&acc_nr and not on reference equality

--- seqscape/models.py
+++ seqscape/models.py
@@ -29,9 +29,8 @@ Base = declarative_base()
 
 
 class Sample(Base):
-    
     __tablename__ = 'current_samples'
-    
+
     internal_id = Column(Integer, primary_key=True)
     name = Column(String)
     accession_number = Column(String)
@@ -43,14 +42,20 @@ class Sample(Base):
     cohort = Column(String)
     country_of_origin = Column(String)
     geographical_region = Column(String)
-    
+
     is_current = Column(Integer)
-    
+
+    def __eq__(self, other):
+        return self.name == other.name and \
+               self.accession_number == other.accession_number and \
+               self.internal_id == other.internal_id
+
+    def __hash__(self):
+        return hash(self.name)
 
 class Study(Base):
-    
     __tablename__ = 'current_studies'
-    
+
     internal_id = Column(Integer, primary_key=True)
     name = Column(String)
     accession_number = Column(String)
@@ -59,29 +64,41 @@ class Study(Base):
     study_title = Column(String)
     study_visibility = Column(String)
     faculty_sponsor = Column(String)
-    
+
     is_current = Column(Integer)
-    
-    
+
+    def __eq__(self, other):
+        return self.name == other.name and \
+               self.accession_number == other.accession_number and \
+               self.internal_id == other.internal_id
+
+    def __hash__(self):
+        return hash(self.name)
+
+
 class Library(Base):
-    
     __tablename__ = 'current_library_tubes'
-    
+
     internal_id = Column(Integer, primary_key=True)
     name = Column(String)
     library_type = Column(String)
-    
+
     is_current = Column(Integer)
-    
+
+    def __eq__(self, other):
+        return self.name == other.name and self.internal_id == other.internal_id
+
+    def __hash__(self):
+        return hash(self.name)
+
 
 class StudySamplesLink(Base):
-    
     __tablename__ = 'current_study_samples'
 
     internal_id = Column(Integer, primary_key=True)
     sample_internal_id = Column(Integer)
     study_internal_id = Column(Integer)
-    
+
     is_current = Column(Integer)
     
     
