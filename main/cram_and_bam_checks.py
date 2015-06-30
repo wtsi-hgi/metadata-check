a5ab05cb5d73e2bc4ff0990c3b77cd91b14411f0
Irina Colgiu <ic4@sanger.ac.uk>
2015-06-30 17:59:18 +0100

    Rewritten most of the module, after refactoring the rest of the modules.

--- main/cram_and_bam_checks.py
+++ main/cram_and_bam_checks.py
@@ -19,27 +19,20 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os
-import argparse
+import arg_parser
 import metadata_utils
-from main import irods_seq_data_tests as seq_tests
+#from main import irods_seq_data_tests as seq_tests
+from main import irods_metadata_consistency_checks as seq_consistency_checks
 from main import error_types
 import complete_irods_metadata_checks
 import irods_metadata_consistency_checks as irods_checks
+import irods_metadata as irods_meta_module
+import header_metadata as header_meta_module
+
 
 CRAM_FILE_TYPE = 'cram'
 BAM_FILE_TYPE = 'bam'
 BOTH_FILE_TYPES = 'both'
-
-# NOT USED - replaced by the tests of complete metadata
-def check_all_identifiers_in_metadata(metadata, name=True, internal_id=True, accession_number=True):
-    error_report = []
-    if name and not metadata.get('name'):
-        error_report.append("NO names in IRODS metadata")
-    if internal_id and not metadata.get('internal_id'):
-        error_report.append("NO  internal_ids in IRODS metadata")
-    if accession_number and not metadata.get('accession_number'):
-        error_report.append("NO accession numbers in IRODS metadata")
-    return error_report
 
 
 def check_irods_vs_header_metadata(irods_path, header_dict, irods_dict, entity_type):
@@ -48,14 +41,15 @@ def check_irods_vs_header_metadata(irods_path, header_dict, irods_dict, entity_t
          irods_dict = dict('name': [sample_name], accession_number: [samples_acc_nr], internal_id: [internal_id])
          header_dict = dict('name': [sample_name], accession_number: [samples_acc_nr], internal_id: [internal_id])
     """
+    problems = []
     for id_type, head_ids_list in header_dict.iteritems():
         if irods_dict.get(id_type) and header_dict.get(id_type):
             if set(head_ids_list).difference(set(irods_dict[id_type])):
-                raise error_types.HeaderVsIrodsMetadataAttributeError(fpath=irods_path, attribute=id_type,
+                problems.append(error_types.HeaderVsIrodsMetadataAttributeError(fpath=irods_path, attribute=id_type,
                                                                       header_value=str(head_ids_list),
                                                                       irods_value=irods_dict[id_type],
-                                                                      entity_type=entity_type)
-
+                                                                      entity_type=entity_type))
+    return problems
 
 
 
@@ -130,7 +124,43 @@ def run_metadata_tests(irods_fpath, irods_metadata, header_metadata=None,
 
 
 
+
+
 def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # INPUT: What files?
+    #parser.add_argument()
+    parser.add_argument('--study', required=False, help='Study name')
+    parser.add_argument('--fpath_irods', required=False, dest='files', help='List of file paths in iRODS', action='append')
+    parser.add_argument('--fofn', required=False,
+                        help='The path to a fofn containing file paths from iRODS '
+                             'for the files one wants to run tests on')
+    parser.add_argument('--sample', required=False, dest='samples', help='Test all the data for this sample', action='append')
+
+    # Filters files by type
+    parser.add_argument('--file_type', choices=['bam', 'cram'], required=False, dest='file_types', action='append',
+                        help='Options are: bam | cram, you can choose more than 1 param.')
+
+
+    # SUBPARSERS - per TEST TYPE:
+    test_sample_parser = argparse.ArgumentParser(parents=[parser])
+
+
+    # OUTPUT: how to output the results?
+
+
+
+#  parent_parser = argparse.ArgumentParser(add_help=False)
+# >>> parent_parser.add_argument('--parent', type=int)
+#
+# >>> foo_parser = argparse.ArgumentParser(parents=[parent_parser])
+# >>> foo_parser.add_argument('foo')
+# >>> foo_parser.parse_args(['--parent', '2', 'XXX'])
+# Namespace(foo='XXX', parent=2)
+
+
+def parse_args2():
     parser = argparse.ArgumentParser()
     # Getting the input (filepaths, etc..)
     parser.add_argument('--study', required=False, help='Study name')
@@ -167,29 +197,28 @@ def parse_args():
     # TODO
 
 
-
     args = parser.parse_args()
-
 
     return args
 
-def check_args(args):
-    if not args.fpaths_irods and not args.study:
-        #parser.print_help()
-        #print "No study provided, no BAM path given => NOTHING TO DO! EXITTING"
-        #exit(0)
-        raise ValueError("No study provided, no BAM path given => NOTHING TO DO! EXITTING")
-    if not args.samples_irods_vs_header and not args.samples_irods_vs_seqscape \
-            and not args.libraries_irods_vs_header \
-            and not args.libraries_irods_vs_seqscape \
-            and not args.study_irods_vs_seqscape\
-            and not args.desired_ref\
-            and not args.collateral_tests\
-            and not args.check_irods_meta_against_config:
-        raise ValueError("You haven't selected neither samples to be checked, nor libraries, nor study. " \
-              "Nothing to be done!")
-        # parser.print_help()
-        # exit(0)
+
+# def check_args(args):
+#     if not args.fpaths_irods and not args.study:
+#         #parser.print_help()
+#         #print "No study provided, no BAM path given => NOTHING TO DO! EXITTING"
+#         #exit(0)
+#         raise ValueError("No study provided, no BAM path given => NOTHING TO DO! EXITTING")
+#     if not args.samples_irods_vs_header and not args.samples_irods_vs_seqscape \
+#             and not args.libraries_irods_vs_header \
+#             and not args.libraries_irods_vs_seqscape \
+#             and not args.study_irods_vs_seqscape\
+#             and not args.desired_ref\
+#             and not args.collateral_tests\
+#             and not args.check_irods_meta_against_config:
+#         raise ValueError("You haven't selected neither samples to be checked, nor libraries, nor study. " \
+#               "Nothing to be done!")
+#         # parser.print_help()
+#         # exit(0)
 
 
 def read_fofn_into_list(fofn_path):
@@ -198,22 +227,24 @@ def read_fofn_into_list(fofn_path):
     fofn_fd.close()
     return files_list
 
+
 def write_list_to_file(input_list, output_file):
     out_fd = open(output_file, 'w')
     for entry in input_list:
         out_fd.write(entry+'\n')
     out_fd.close()
 
+
 def collect_fpaths_for_study(study, file_type=BOTH_FILE_TYPES):
     fpaths_irods = []
     if file_type == CRAM_FILE_TYPE:
         fpaths_irods = metadata_utils.iRODSUtils.retrieve_list_of_crams_by_study_from_irods(study)
-        print "NUMBER of CRAMs found: " + str(len(fpaths_irods))
-        write_list_to_file(fpaths_irods, 'hiv-crams.out')
+        #print "NUMBER of CRAMs found: " + str(len(fpaths_irods))
+        #write_list_to_file(fpaths_irods, 'hiv-crams.out')
     elif file_type == BAM_FILE_TYPE:
         fpaths_irods = metadata_utils.iRODSUtils.retrieve_list_of_bams_by_study_from_irods(study)
-        print "NUMBER of BAMs found: " + str(len(fpaths_irods))
-        write_list_to_file(fpaths_irods, 'hiv-bams.out')
+        #print "NUMBER of BAMs found: " + str(len(fpaths_irods))
+        #write_list_to_file(fpaths_irods, 'hiv-bams.out')
     elif file_type == BOTH_FILE_TYPES:
         bams_fpaths_irods = metadata_utils.iRODSUtils.retrieve_list_of_bams_by_study_from_irods(study)
         crams_fpaths_irods = metadata_utils.iRODSUtils.retrieve_list_of_crams_by_study_from_irods(study)
@@ -224,6 +255,7 @@ def collect_fpaths_for_study(study, file_type=BOTH_FILE_TYPES):
         print "CRAMs: " + str(crams_fpaths_irods)
     return fpaths_irods
 
+
 def collect_fpaths_from_args(study=None, file_type=BOTH_FILE_TYPES, files_list=None, fofn_path=None):
     if study:
         fpaths_irods = collect_fpaths_for_study(study, file_type)
@@ -232,6 +264,158 @@ def collect_fpaths_from_args(study=None, file_type=BOTH_FILE_TYPES, files_list=N
     elif files_list:
         fpaths_irods = files_list
     return fpaths_irods
+
+def collect_fpaths_for_samples(samples):
+    pass
+
+def filter_by_file_type(fpaths, file_type):
+    return [f for f in fpaths if f.endswith(file_type)]
+
+def filter_by_avu(fpath, avu_attribute, avu_value):
+    pass# manual_qc,...
+
+def main():
+    args = arg_parser.parse_args()
+
+    # COLLECT FILE PATHS:
+    fpaths_per_type = {} # type : [ fpath ]
+    if args.study:
+    #     if BAM_FILE_TYPE in args.file_types:
+    #         fpaths_per_type[BAM_FILE_TYPE] = metadata_utils.iRODSUtils.retrieve_list_of_bams_by_study_from_irods(args.study)
+    #     if CRAM_FILE_TYPE in args.file_types:
+    #         fpaths_per_type[CRAM_FILE_TYPE] = metadata_utils.iRODSUtils.retrieve_list_of_crams_by_study_from_irods(args.study)
+    # fpaths = fpaths_per_type.get(BAM_FILE_TYPE) + fpaths_per_type.get(CRAM_FILE_TYPE)
+
+        fpaths = metadata_utils.retrieve_list_of_files_by_study(args.study)
+
+    if args.fofn:
+        fpaths.extend(read_fofn_into_list(args.fofn))
+
+    if args.files:
+        fpaths = args.files
+
+    # per samples:
+    if args.fosn:
+        samples = read_fofn_into_list(args.fosn)
+        # query irods for samples, get a list of files
+
+    if args.samples:
+        samples = args.samples
+        # query irods per sample for files
+        pass
+
+    # Check for conflicts in the params?
+    # TODO
+
+    # FILTER FILES:
+    filtered_fpaths = []
+    for file_type in args.file_types:
+        print "FILE types: " + str(fpaths)
+        filtered_fpaths.extend(filter_by_file_type(fpaths, file_type))
+
+
+    # PREPARING FOR THE TESTS
+    for f in fpaths:
+        problems = []
+        if args.config_file:
+            pass
+
+
+        # Deciding what tests to run
+        header_tests = False
+        if 'irods_vs_header' in args.test_sample or 'irods_vs_header' in args.test_library or 'all' in args.test_sample or 'all' in args.test_library:
+            header_tests = True
+
+        irods_tests = False
+        if any([args.test_sample, args.test_library, args.test_study, args.test_reference, args.test_md5, args.test_filename, args.all_tests]):
+            irods_tests = True
+
+        # Retrieve the resources as needed in preparation for the tests:
+        h_meta = None
+        if header_tests:
+            header = metadata_utils.HeaderUtils.get_parsed_header_from_irods_file(f)
+            h_meta = header_meta_module.HeaderSAMFileMetadata.from_header_to_metadata(header, f)
+
+
+        if irods_tests:
+            irods_avus = metadata_utils.iRODSUtils.retrieve_irods_avus(f)
+            avu_issues = irods_meta_module.IrodsSeqFileMetadata.run_avu_count_checks(f, irods_avus)
+            problems.extend(avu_issues)
+
+            i_meta = irods_meta_module.IrodsSeqFileMetadata.from_avus_to_irods_metadata(irods_avus, f)
+            sanity_issues = i_meta.run_field_sanity_checks()
+            problems.extend(sanity_issues)
+
+            ####### RUN THE TESTS: #########
+            # MD5 tests:
+            if args.test_md5:
+                try:
+                    i_meta.test_md5_calculated_vs_metadata()
+                except (error_types.WrongMD5Error, error_types.TestImpossibleToRunError) as e:
+                    problems.append(str(e))
+
+            # TODO - not working properly
+            if args.test_reference:
+                try:
+                    i_meta.test_reference(args.test_reference)
+                except (error_types.WrongReferenceError, error_types.TestImpossibleToRunError) as e:
+                    problems.append(str(e))
+
+            if args.test_filename:
+                try:
+                    i_meta.test_lane_from_fname_vs_metadata()
+                except (error_types.IrodsMetadataAttributeVsFileNameError, error_types.TestImpossibleToRunError) as e:
+                    problems.append(str(e))
+
+                try:
+                    i_meta.test_run_id_from_fname_vs_metadata()
+                except (error_types.IrodsMetadataAttributeVsFileNameError, error_types.TestImpossibleToRunError) as e:
+                    problems.append(str(e))
+
+                # TODO : test also the tag
+
+            if args.test_sample:
+                if 'all' in args.test_sample:
+                    print "samples in h_meta: " + str(h_meta.samples) + " and i meta: " + str(i_meta.samples)
+                    issues = check_irods_vs_header_metadata(f, h_meta.samples, i_meta.samples, 'sample')
+                    problems.extend(issues)
+
+                    issues = seq_consistency_checks.compare_entity_sets_in_seqsc(i_meta.samples, 'sample')    #def compare_entity_sets_in_seqsc(entities_dict, entity_type):
+                    problems.extend(issues)
+                else:
+                    if 'irods_vs_header' in args.test_sample:
+                        issues = check_irods_vs_header_metadata(f, h_meta.samples, i_meta.samples, 'sample')
+                        problems.extend(issues)
+
+                    if 'irods_vs_seqsc' in args.test_sample:
+                        issues = seq_consistency_checks.compare_entity_sets_in_seqsc(i_meta.samples, 'sample')    #def compare_entity_sets_in_seqsc(entities_dict, entity_type):
+                        problems.extend(issues)
+
+            if args.test_library:
+                if 'all' in args.test_library:
+                    issues = check_irods_vs_header_metadata(f, h_meta.libraries, i_meta.libraries, 'library')
+                    problems.extend(issues)
+
+                    issues = seq_consistency_checks.compare_entity_sets_in_seqsc(i_meta.libraries, 'library')
+                    problems.extend(issues)
+                else:
+                    if 'irods_vs_header' in args.test_library:
+                        issues = check_irods_vs_header_metadata(f, h_meta.libraries, i_meta.libraries, 'library')
+                        problems.extend(issues)
+
+                    if 'irods_vs_seqsc' in args.test_library:
+                        issues = seq_consistency_checks.compare_entity_sets_in_seqsc(i_meta.libraries, 'library')
+                        problems.extend(issues)
+
+
+            if args.test_study:
+                issues = seq_consistency_checks.compare_entity_sets_in_seqsc(i_meta.studies, 'study')
+                problems.extend(issues)
+
+        print "Problems found: " + str(problems)
+
+
+    # OUTPUTS
 
 
 def start_tests(study=None, file_type='both', fpaths=None, fofn_path=None, samples_irods_vs_header=True, samples_irods_vs_seqscape=True,
@@ -247,8 +431,8 @@ def start_tests(study=None, file_type='both', fpaths=None, fofn_path=None, sampl
         #print "FPATH analyzed: " + str(fpath)
 
         if samples_irods_vs_header or libraries_irods_vs_header:
-            irods_metadata = metadata_utils.iRODSUtils.retrieve_irods_metadata(fpath)
-            header_metadata = metadata_utils.HeaderUtils.get_header_metadata_from_irods_file(fpath)
+            irods_metadata = metadata_utils.iRODSUtils.retrieve_irods_avus(fpath)
+            header_metadata = metadata_utils.HeaderUtils.get_parsed_header_from_irods_file(fpath)
             if irods_meta_conf:
 
                 pass
@@ -262,7 +446,7 @@ def start_tests(study=None, file_type='both', fpaths=None, fofn_path=None, sampl
     #print "FILES PER TYPE: CRAMs = " + str(nr_crams) + " and BAMs = " + str(nr_bams)
 
 # TODO: write in README - actually all these tests apply only to irods seq data...
-def main():
+def main1():
     args = parse_args()
     start_tests(args.study, args.file_type, args.fpaths_irods, args.fofn, args.samples_irods_vs_header, args.samples_irods_vs_seqscape,
                 args.libraries_irods_vs_header, args.libraries_irods_vs_seqscape, args.study_irods_vs_seqscape,
