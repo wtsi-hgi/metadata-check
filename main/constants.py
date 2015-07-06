fd612f39bf84ec53bc502619164d398a53f8e53d
IrinaColgiu <irina.colgiu@gmail.com>
2015-07-06 13:22:51 +0100

    Moved the reference names into constants.py

---
+++ main/constants.py
@@ -0,0 +1,26 @@
+"""
+Copyright (C) 2015  Genome Research Ltd.
+
+Author: Irina Colgiu <ic4@sanger.ac.uk>
+
+This program is part of metadata-check
+
+metadata-check is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+This file has been created on Jun 30, 2015.
+"""
+
+# TODO: add the regferences and file types here
+HS37D5 = 'hs37d5'
+GRCH38 = 'GRCh38'
+GRCH37 = 'GRCh37'
+G1K = 'human_g1k_v37'
