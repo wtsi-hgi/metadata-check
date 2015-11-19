e48e9ca15819c90bed6a32d376f19e5f4b332f44
IrinaColgiu <irina.colgiu@gmail.com>
2015-11-19 16:57:59 +0000

    Moved code getting metadata from cram_to_bam.py to here

--- metadata_provider/irods_meta_provider.py
+++ metadata_provider/irods_meta_provider.py
@@ -19,7 +19,24 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 This file has been created on Nov 16, 2015.
 """
 
+from main import metadata_utils
+from irods_baton import baton_wrapper as baton
+
 class iRODSMetadataProvider:
+
+    def get_files_and_metadata_by_metadata(search_criteria):
+        metaquery_results = baton.BatonAPI.query_by_metadata_and_get_results_as_json(search_criteria) # avu_tuple_list
+        fpaths_checksum_and_avus = metadata_utils.iRODSBatonUtils.from_metaquery_results_to_fpaths_and_avus(metaquery_results)  # this is a dict of key = fpath, value = dict({'avus':[], 'checksum':str})
+        print("NR OF FPATHS FOUND: " + str(len(fpaths_checksum_and_avus)))
+        return fpaths_checksum_and_avus
+
+
+    def get_files_and_metadata_by_fpath(fpaths):
+        fpaths_checksum_and_avus = baton.BatonAPI.get_all_files_metadata(fpaths)
+        fpaths_checksum_and_avus = [_f for _f in fpaths_checksum_and_avus.split('\n') if _f]
+        #fpaths_checksum_and_avus = metadata_utils.iRODSBatonUtils.from_metaquery_results_to_fpaths_and_avus(fpaths_checksum_and_avus)  # this is a dict of key = fpath, value = dict({'avus':[], 'checksum':str})
+        fpaths_checksum_and_avus = metadata_utils.iRODSBatonUtils.from_multi_metaquery_results_to_fpaths_and_avus(fpaths_checksum_and_avus)
+        return fpaths_checksum_and_avus
 
     @classmethod
     def retrieve_metadata_by_file_path(cls, fpath):
