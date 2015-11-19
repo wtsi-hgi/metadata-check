1e235c9f318f6e48070ee141c97da73cdb14d47d
Irina Colgiu <ic4@sanger.ac.uk>
2015-11-19 16:53:29 +0000

    Moved code belonging to IrodsMetadata class for refactoring

--- main/metadata_utils.py
+++ main/metadata_utils.py
@@ -19,16 +19,16 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 This file has been created on Feb 10, 2015.
 """
 
-from . import irods_metadata
+import os
+from collections import defaultdict
+
 from irods import api as irods_api
 from irods import icommands_wrapper
 from irods import data_types
 from header_parser import sam_header_analyser as header_analyser
-import os
-from .identifiers import EntityIdentifier as Identif
+from metadata_types.identifiers import EntityIdentifier as Identif
 from com import  utils as common_utils
 from . import error_types
-from collections import defaultdict
 
 
 class GeneralUtils:
@@ -133,8 +133,11 @@ class iRODSUtils(object):
 
 
 
+
+
+
 import json
-import ijson
+#import ijson
 
 class iRODSBatonUtils(iRODSUtils):
 
@@ -338,48 +341,4 @@ class iRODSiCmdsUtils(iRODSUtils):
     def retrieve_irods_avus(cls, irods_path):
         return irods_api.iRODSAPI.retrieve_metadata_for_file(irods_path)
 
-    @classmethod
-    def extract_values_for_key_from_irods_metadata(cls, avus_list, key):
-        results = []
-        for avu in avus_list:
-            if avu.attribute == key:
-                results.append(avu.value)
-        return results
-
-
-    @classmethod
-    def extract_samples_from_irods_metadata(cls, irods_metadata):
-        irods_sample_names_list = cls.extract_values_for_key_from_irods_metadata(irods_metadata, 'sample')
-        irods_sample_acc_nr_list = cls.extract_values_for_key_from_irods_metadata(irods_metadata, 'sample_accession_number')
-        irods_sample_internal_id_list = cls.extract_values_for_key_from_irods_metadata(irods_metadata, 'sample_id')
-        irods_samples = {'name': irods_sample_names_list,
-                         'accession_number': irods_sample_acc_nr_list,
-                         'internal_id': irods_sample_internal_id_list
-        }
-        return irods_samples
-
-    @classmethod
-    def extract_studies_from_irods_metadata(cls, irods_metadata):
-        irods_study_names_list = cls.extract_values_for_key_from_irods_metadata(irods_metadata, 'study')
-        irods_study_internal_id_list = cls.extract_values_for_key_from_irods_metadata(irods_metadata, 'study_id')
-        irods_study_acc_nr_list = cls.extract_values_for_key_from_irods_metadata(irods_metadata, 'study_accession_number')
-        irods_studies = {'name': irods_study_names_list,
-                         'internal_id': irods_study_internal_id_list,
-                         'accession_number': irods_study_acc_nr_list
-        }
-        return irods_studies
-
-
-    @classmethod
-    def extract_libraries_from_irods_metadata(cls, irods_metadata):
-        irods_lib_internal_id_list = cls.extract_values_for_key_from_irods_metadata(irods_metadata, 'library_id')
-        irods_lib_names_list = cls.extract_values_for_key_from_irods_metadata(irods_metadata, 'library')
-
-        # HACKS for the inconsistencies in iRODS, in which NPG submits under library name the actual library id
-        lib_ids = list(set(irods_lib_internal_id_list + irods_lib_names_list))
-        ids_dict = {'name': [], 'accession_number': [], 'internal_id': []}
-        for id in lib_ids:
-            id_type = Identif.guess_identifier_type(id)
-            ids_dict[id_type].append(id)
-        return ids_dict
 
