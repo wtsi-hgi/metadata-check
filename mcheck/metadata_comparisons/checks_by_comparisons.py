c49303d664e149a556a0fbfd788ce6c63f398741
IrinaColgiu <irina.colgiu@gmail.com>
2016-04-15 12:26:23 +0100

    Updated check comparisons

--- mcheck/metadata_comparisons/checks_by_comparisons.py
+++ mcheck/metadata_comparisons/checks_by_comparisons.py
@@ -19,8 +19,12 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 This file has been created on Jan 12, 2016.
 """
 
+import config
 from mcheck.metadata.seqscape_metadata.seqscape_meta_provider import SeqscapeRawMetadataProvider
 from mcheck.metadata.seqscape_metadata.seqscape_metadata import SeqscapeEntityQueryAndResults, SeqscapeRawMetadata, SeqscapeMetadata
+from mcheck.metadata.file_header_metadata.header_meta_provider import SAMFileHeaderMetadataProvider
+from mcheck.metadata.irods_metadata.irods_meta_provider import iRODSMetadataProvider
+
 
 class IrodsMetadataVsHeaderMetadata:
 
@@ -29,17 +33,28 @@ class IrodsMetadataVsHeaderMetadata:
 
 
 
-class Setup:
+class MetadataRetrieval:
 
-    def fetch_seqscape_metadata(self, samples, libraries, studies):
+    @staticmethod
+    def fetch_seqscape_metadata(samples, libraries, studies):
         raw_metadata = SeqscapeRawMetadataProvider.fetch_raw_metadata(samples, libraries, studies)
         problems = raw_metadata.check_raw_metadata()
         seqsc_metadata = SeqscapeMetadata.from_raw_metadata(raw_metadata)
         return seqsc_metadata, problems
 
-    def fetch_header_metadata(self):
-        pass
+    @staticmethod
+    def fetch_header_metadata(fpath):
+        return SAMFileHeaderMetadataProvider.fetch(fpath, irods=True)
 
-    def fetch_irods_metadata(self):
-        pass
+    @staticmethod
+    def fetch_irods_metadata(fpath):
+        return iRODSMetadataProvider.retrieve_metadata_by_file_path(fpath)
 
+
+fpath = '/seq/illumina/library_merge/13841100.CCXX.paired310.4199421624/13841100.CCXX.paired310.4199421624.cram'
+h_meta = MetadataRetrieval.fetch_header_metadata(fpath)
+seqsc_meta, errs = MetadataRetrieval.fetch_seqscape_metadata(samples=h_meta.samples, libraries=h_meta.libraries, studies=h_meta.studies )
+irods_meta = MetadataRetrieval.fetch_irods_metadata(fpath)
+print("H METAAAAA: %s\n" % h_meta)
+print("Seqscape meta: %s\n" % str(seqsc_meta))
+print("Irods meta: %s\n" % irods_meta)
