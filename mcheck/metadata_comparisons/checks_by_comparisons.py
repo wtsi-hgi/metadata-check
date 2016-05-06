c497f7749065332557952e8f7d4ebde63cd5c651
IrinaColgiu <ic4@sanger.ac.uk>
2016-05-06 15:48:19 +0100

    Implemented fetch*metadata

--- mcheck/metadata_comparisons/checks_by_comparisons.py
+++ mcheck/metadata_comparisons/checks_by_comparisons.py
@@ -20,41 +20,74 @@ This file has been created on Jan 12, 2016.
 """
 
 import config
+from typing import Set, Dict
+from sequencescape import NamedModel, Sample, Study, Library
+
 from mcheck.metadata.seqscape_metadata.seqscape_meta_provider import SeqscapeRawMetadataProvider
 from mcheck.metadata.seqscape_metadata.seqscape_metadata import SeqscapeEntityQueryAndResults, SeqscapeRawMetadata, SeqscapeMetadata
 from mcheck.metadata.file_header_metadata.header_meta_provider import SAMFileHeaderMetadataProvider
 from mcheck.metadata.irods_metadata.irods_meta_provider import iRODSMetadataProvider
+from mcheck.metadata.irods_metadata.irods_file_metadata import IrodsSeqFileMetadata
 
 
 class IrodsMetadataVsHeaderMetadata:
 
-    def compare(self, irods_metadata, header_metadata):
-        pass
+    pass
+
+    # def compare(self, irods_metadata, header_metadata):
+    #     return self._compare_entities(irods_metadata.samples, header_metadata.samples)
 
 
+class FileMetadataComparison:
 
-class MetadataRetrieval:
+    @staticmethod
+    def compare_entities(entity_set1: Dict[str, str], entity_set2: Dict[str, str]):
+        """
+        Compares the entities in 2 different dicts that look like: {'accession_number': {'EGAN00001099700'}, 'name': {'SC_SEPI5488478'}, 'internal_id': {'1582333'}}
+        :param entity_set1: dict of key = id_type, value = id_value
+        :param entity_set2: dict of key = id_type, value = id_value
+        :return:
+        """
+        for id_type, values in entity_set1.items():
+            if values and entity_set2.get(id_type):
+                if values != entity_set2.get(id_type):
+                    return False
+        return True
+
+class BulkMetadataRetrieval:
+
+    @staticmethod
+    def fetch_irods_metadata_by_metadata(search_criteria):
+        """
+        Queries iRODS for all the files that match the search criteria and fetch all the metadata for them.
+        :param search_criteria: a dict with: key = search field name, value = search field value
+        :return:
+        """
+        return iRODSMetadataProvider.retrieve_fileinfo_and_metadata_by_metadata(search_criteria)
+
+
+class FileMetadataRetrieval:
 
     @staticmethod
     def fetch_seqscape_metadata(samples, libraries, studies):
         raw_metadata = SeqscapeRawMetadataProvider.fetch_raw_metadata(samples, libraries, studies)
-        problems = raw_metadata.check_raw_metadata()
+        problems = raw_metadata.check_metadata()
         seqsc_metadata = SeqscapeMetadata.from_raw_metadata(raw_metadata)
         return seqsc_metadata, problems
 
     @staticmethod
     def fetch_header_metadata(fpath):
-        return SAMFileHeaderMetadataProvider.fetch(fpath, irods=True)
+        header_metadata = SAMFileHeaderMetadataProvider.fetch_metadata(fpath, irods=True)
+        problems = header_metadata.check_metadata()
+        header_metadata.fix_metadata()
+        return header_metadata, problems
 
     @staticmethod
-    def fetch_irods_metadata(fpath):
-        return iRODSMetadataProvider.retrieve_metadata_by_file_path(fpath)
+    def fetch_irods_metadata_by_path(fpath):
+        raw_metadata = iRODSMetadataProvider.fetch_raw_metadata(fpath)
+        problems = raw_metadata.check_metadata()
+        seq_metadata = IrodsSeqFileMetadata.from_raw_metadata(raw_metadata)
+        return seq_metadata, problems
 
 
-fpath = '/seq/illumina/library_merge/13841100.CCXX.paired310.4199421624/13841100.CCXX.paired310.4199421624.cram'
-h_meta = MetadataRetrieval.fetch_header_metadata(fpath)
-seqsc_meta, errs = MetadataRetrieval.fetch_seqscape_metadata(samples=h_meta.samples, libraries=h_meta.libraries, studies=h_meta.studies )
-irods_meta = MetadataRetrieval.fetch_irods_metadata(fpath)
-print("H METAAAAA: %s\n" % h_meta)
-print("Seqscape meta: %s\n" % str(seqsc_meta))
-print("Irods meta: %s\n" % irods_meta)
+
