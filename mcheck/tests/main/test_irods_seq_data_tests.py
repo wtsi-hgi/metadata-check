3752233d3b08dca058803f92146acd89f1ccd61d
IrinaColgiu <ic4@sanger.ac.uk>
2016-05-16 10:50:35 +0100

    Updated the import of data_type to avu

--- mcheck/tests/main/test_irods_seq_data_tests.py
+++ mcheck/tests/main/test_irods_seq_data_tests.py
@@ -24,7 +24,7 @@ import unittest
 import config
 from mcheck.main import irods_seq_data_tests as irods_seq_tests
 from mcheck.main import error_types
-from mcheck.metadata.irods_metadata import data_types as irods_types
+from mcheck.metadata.irods_metadata import avu
 
 
 @unittest.skip
@@ -33,7 +33,7 @@ class MD5Test(unittest.TestCase):
     @unittest.skipIf(config.RUNNING_LOCATION == 'localhost', "Skipping checksum test because it runs locally")
     def test_check_bam_md5_metadata(self):
         irods_fpath = '/seq/14761/14761_4.bam'
-        irods_metadata = [irods_types.MetaAVU(attribute='md5', value='df8afb9196ea7080cd261e65d4ab2ef9')]
+        irods_metadata = [avu.MetaAVU(attribute='md5', value='df8afb9196ea7080cd261e65d4ab2ef9')]
         result = irods_seq_tests.check_md5_metadata(irods_fpath, irods_metadata)
         print(result)
         self.assertEqual(len(result), 1)
@@ -41,7 +41,7 @@ class MD5Test(unittest.TestCase):
     @unittest.skipIf(config.RUNNING_LOCATION == 'localhost', "Skipping checksum test because it runs locally")
     def test_check_cram_md5_metadata(self):
         irods_fpath = '/seq/14761/14761_4.cram'
-        irods_metadata = [irods_types.MetaAVU(attribute='md5', value='a58d8e78d5d7a2a55136448a81c5dd90')]
+        irods_metadata = [avu.MetaAVU(attribute='md5', value='a58d8e78d5d7a2a55136448a81c5dd90')]
         result = irods_seq_tests.check_md5_metadata(irods_fpath, irods_metadata)
         self.assertEqual(len(result), 0)
 
@@ -50,7 +50,7 @@ class AdjancentMetadataChecksTests(unittest.TestCase):
 
     def test_check_bam_run_id(self):
         # Testing it works for normal cases:
-        irods_metadata = [irods_types.MetaAVU('id_run', '11000')]
+        irods_metadata = [avu.MetaAVU('id_run', '11000')]
         irods_fpath = '/seq/11000/11000_2#3.bam'
         result = irods_seq_tests.check_run_id(irods_fpath, irods_metadata)
         expected = []
@@ -63,20 +63,20 @@ class AdjancentMetadataChecksTests(unittest.TestCase):
         self.assertEqual(len(result), 1)
 
         # Testing that it returns an error message if there is more than 1 run in the metadata:
-        irods_metadata = [irods_types.MetaAVU('id_run', '11000'), irods_types.MetaAVU('id_run', '11111')]
+        irods_metadata = [avu.MetaAVU('id_run', '11000'), avu.MetaAVU('id_run', '11111')]
         irods_fpath = '/seq/11000/11000_2#3.bam'
         result = irods_seq_tests.check_run_id(irods_fpath, irods_metadata)
         self.assertEqual(len(result), 1)
 
         # Testing that it returns an error message if the irods path doesn't look like expected:
-        irods_metadata = [irods_types.MetaAVU('id_run', '11000')]
+        irods_metadata = [avu.MetaAVU('id_run', '11000')]
         irods_fpath = '/humgen/projects/hgi/11000_2#3.bam'
         result = irods_seq_tests.check_run_id(irods_fpath, irods_metadata)
         self.assertEqual(len(result), 1)
 
     def test_check_cram_run_id(self):
         # Testing it works for normal cases:
-        irods_metadata = [irods_types.MetaAVU('id_run', '11000')]
+        irods_metadata = [avu.MetaAVU('id_run', '11000')]
         irods_fpath = '/seq/11000/11000_2#3.cram'
         result = irods_seq_tests.check_run_id(irods_fpath, irods_metadata)
         expected = []
@@ -89,13 +89,13 @@ class AdjancentMetadataChecksTests(unittest.TestCase):
         self.assertEqual(len(result), 1)
 
         # Testing that it returns an error message if there is more than 1 run in the metadata:
-        irods_metadata = [irods_types.MetaAVU('id_run', '11000'), irods_types.MetaAVU('id_run', '11111')]
+        irods_metadata = [avu.MetaAVU('id_run', '11000'), avu.MetaAVU('id_run', '11111')]
         irods_fpath = '/seq/11000/11000_2#3.cram'
         result = irods_seq_tests.check_run_id(irods_fpath, irods_metadata)
         self.assertEqual(len(result), 1)
 
         # Testing that it returns an error message if the irods path doesn't look like expected:
-        irods_metadata = [irods_types.MetaAVU('id_run', '11000')]
+        irods_metadata = [avu.MetaAVU('id_run', '11000')]
         irods_fpath = '/humgen/projects/hgi/11000_2#3.cram'
         result = irods_seq_tests.check_run_id(irods_fpath, irods_metadata)
         self.assertEqual(len(result), 1)
@@ -104,21 +104,21 @@ class AdjancentMetadataChecksTests(unittest.TestCase):
 
     def test_check_bam_lane_metadata(self):
         # Testing on a normal case - lanelet:
-        irods_metadata = [irods_types.MetaAVU('lane', '1')]
+        irods_metadata = [avu.MetaAVU('lane', '1')]
         irods_fpath = '/seq/11111/11111_1#2.bam'
         result = irods_seq_tests.check_lane_metadata(irods_fpath, irods_metadata)
         expected = []
         self.assertEqual(result, expected)
 
         # Testing on a lane-level BAM:
-        irods_metadata = [irods_types.MetaAVU('lane', '2')]
+        irods_metadata = [avu.MetaAVU('lane', '2')]
         irods_fpath = '/seq/11111/11111_2.bam'
         result = irods_seq_tests.check_lane_metadata(irods_fpath, irods_metadata)
         expected = []
         self.assertEqual(result, expected)
 
         # Testing that a message is returned if the irods path doesn't look normal:
-        irods_metadata = [irods_types.MetaAVU('lane', '2')]
+        irods_metadata = [avu.MetaAVU('lane', '2')]
         irods_fpath = '/humgen/projects/hgi/SSS123.bam'
         result = irods_seq_tests.check_lane_metadata(irods_fpath, irods_metadata)
         self.assertEqual(len(result), 1)
@@ -132,21 +132,21 @@ class AdjancentMetadataChecksTests(unittest.TestCase):
 
     def test_check_cram_lane_metadata(self):
         # Testing on a normal case - lanelet:
-        irods_metadata = [irods_types.MetaAVU('lane', '1')]
+        irods_metadata = [avu.MetaAVU('lane', '1')]
         irods_fpath = '/seq/11111/11111_1#2.cram'
         result = irods_seq_tests.check_lane_metadata(irods_fpath, irods_metadata)
         expected = []
         self.assertEqual(result, expected)
 
         # Testing on a lane-level BAM:
-        irods_metadata = [irods_types.MetaAVU('lane', '2')]
+        irods_metadata = [avu.MetaAVU('lane', '2')]
         irods_fpath = '/seq/11111/11111_2.cram'
         result = irods_seq_tests.check_lane_metadata(irods_fpath, irods_metadata)
         expected = []
         self.assertEqual(result, expected)
 
         # Testing that a message is returned if the irods path doesn't look normal:
-        irods_metadata = [irods_types.MetaAVU('lane', '2')]
+        irods_metadata = [avu.MetaAVU('lane', '2')]
         irods_fpath = '/humgen/projects/hgi/SSS123.cram'
         result = irods_seq_tests.check_lane_metadata(irods_fpath, irods_metadata)
         self.assertEqual(len(result), 1)
@@ -160,13 +160,13 @@ class AdjancentMetadataChecksTests(unittest.TestCase):
 
     def test_check_reference(self):
         # Testing on a normal case - the ref is not the desired one:
-        irods_metadata = [irods_types.MetaAVU('reference', '/path/refs/GRCh38_15/all/bwa/Homo_sapiens.GRCh38_15.fa')]
+        irods_metadata = [avu.MetaAVU('reference', '/path/refs/GRCh38_15/all/bwa/Homo_sapiens.GRCh38_15.fa')]
         desired_ref = 'hs37d5'
         result = self.assertRaises(error_types.WrongReferenceError, irods_seq_tests.check_reference, '/seq/path/doesnt/matter.bam', irods_metadata, desired_ref)
         #self.assertEqual(len(result), 1)
 
         # Testing on a normal case - the ref is the desired one:
-        irods_metadata = [irods_types.MetaAVU('reference', '/path/ref/Homo_sapiens/1000Genomes_hs37d5/all/bwa/hs37d5.fa')]
+        irods_metadata = [avu.MetaAVU('reference', '/path/ref/Homo_sapiens/1000Genomes_hs37d5/all/bwa/hs37d5.fa')]
         desired_ref = 'hs37d5'
         result = irods_seq_tests.check_reference('/seq/path/doesnt/matter.bam', irods_metadata, desired_ref)
         self.assertEqual(len(result), 0)
