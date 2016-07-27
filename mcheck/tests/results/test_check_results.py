4c88624fa71bd9634b52c62218cfd71c49921926
IrinaColgiu <ic4@sanger.ac.uk>
2016-07-27 16:53:01 +0100

    Fixed names

--- mcheck/tests/results/test_check_results.py
+++ mcheck/tests/results/test_check_results.py
@@ -1,8 +1,7 @@
 import json
 import unittest
 
-from mcheck.results.checks_results import  CheckResult, RESULT_JSON_PROPERTY, \
-    EXECUTED_JSON_PROPERTY, ERROR_MESSAGE_JSON_PROPERTY, SEVERITY_JSON_PROPERTY, CHECK_NAME_JSON_PROPERTY
+from mcheck.results.checks_results import  CheckResult
     #CheckResultJSONDecoder, CheckResultJSONEncoder,
 from mcheck.results.constants import RESULT, SEVERITY
 from hgijson import MappingJSONEncoderClassBuilder, MappingJSONDecoderClassBuilder
@@ -26,16 +25,16 @@ class TestCheckResultJSONEncoder(unittest.TestCase):
     def test_with_all_properties_set(self):
         check_result_as_json_str = json.dumps(self.check_result, cls=self.CheckResultJSONEncoder)
         check_result_as_json_dict = json.loads(check_result_as_json_str)
-        self.assertEqual(check_result_as_json_dict[CHECK_NAME_JSON_PROPERTY], self.check_result.check_name)
-        self.assertEqual(check_result_as_json_dict[SEVERITY_JSON_PROPERTY], self.check_result.severity)
-        self.assertEqual(check_result_as_json_dict[ERROR_MESSAGE_JSON_PROPERTY], self.check_result.error_message)
-        self.assertEqual(check_result_as_json_dict[EXECUTED_JSON_PROPERTY], self.check_result.executed)
-        self.assertEqual(check_result_as_json_dict[RESULT_JSON_PROPERTY], self.check_result.result)
+        self.assertEqual(check_result_as_json_dict["check_name"], self.check_result.check_name)
+        self.assertEqual(check_result_as_json_dict["severity"], self.check_result.severity)
+        self.assertEqual(check_result_as_json_dict["error_message"], self.check_result.error_message)
+        self.assertEqual(check_result_as_json_dict["executed"], self.check_result.executed)
+        self.assertEqual(check_result_as_json_dict["result"], self.check_result.result)
 
     def test_with_no_error_message(self):
         self.check_result.error_message = None
         check_result_as_json_dict = self.CheckResultJSONEncoder().default(self.check_result)
-        self.assertNotIn(ERROR_MESSAGE_JSON_PROPERTY, check_result_as_json_dict)
+        self.assertNotIn("error_message", check_result_as_json_dict)
 
     def test_with_list(self):
         check_results = [self.check_result for _ in range(10)]
@@ -49,11 +48,11 @@ class TestCheckResultJSONDecoder(unittest.TestCase):
     """
     def setUp(self):
         self.check_result_as_json_dict = {
-            CHECK_NAME_JSON_PROPERTY: _NAME,
-            EXECUTED_JSON_PROPERTY: _EXEUCTED,
-            RESULT_JSON_PROPERTY: _RESULT,
-            SEVERITY_JSON_PROPERTY: _SEVERITY,
-            ERROR_MESSAGE_JSON_PROPERTY: _ERROR_MESSAGE
+            "check_name": _NAME,
+            "executed": _EXEUCTED,
+            "result": _RESULT,
+            "severity": _SEVERITY,
+            "error_message": _ERROR_MESSAGE
         }
         self.CheckResultJSONDecoder = MappingJSONDecoderClassBuilder(CheckResult, CheckResult.to_json_mapping()).build()
 
@@ -68,7 +67,7 @@ class TestCheckResultJSONDecoder(unittest.TestCase):
         self.assertEqual(check_result.result, _RESULT)
 
     def test_with_no_error_message(self):
-        del self.check_result_as_json_dict[ERROR_MESSAGE_JSON_PROPERTY]
+        del self.check_result_as_json_dict["error_message"]
         check_result_as_json_str = json.dumps(self.check_result_as_json_dict)
         check_result = json.loads(check_result_as_json_str, cls=self.CheckResultJSONDecoder)
         self.assertIsNone(check_result.error_message)
